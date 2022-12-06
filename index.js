760a35a9f4edc3ecd6a639a474fb54ca886a4549
Hykky26 <117548943+Hykky26@users.noreply.github.com>
2022-12-06 22:27:21 +0100

    Feat function playRound simulate one round of the game

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-/* creat a function getCumputeChoice ()that return randomly either 'Rock' 'Paper' or 'Scissors'
- */
-
-/* creat a function with 2 param playerSelection && cumputerSelection that return a string that declare the winner as "You lose ! Paper Beats Rock";*/
-
-// write a function game()
-// call the playRound() inside of it to play 5 round && keeps score && report a winner or loser at the end
-
-//génére un nombre aléatoire en 0 et 3
-// const choice = Math.floor(Math.random() * 4)
-
 const cumputerChoice = Math.floor(Math.random() * 4);
-const playerChoice = prompt("Rock, Paper , Scissors ?");
+
+const Rock = "Rock";
+const Paper = "Paper";
+const Scissors = "Scissors";
 
 function getCumputerChoice() {
   if (cumputerChoice === 3) {
-    console.log("Rock");
+    return Rock;
   } else if (cumputerChoice === 2) {
-    console.log("Paper");
+    return Paper;
   } else {
-    console.log("Scissors");
+    return Scissors;
   }
 }
 
 function playRound(playerSelection, cumputerSelection) {
-  return playerSelection && cumputerSelection;
+  if (playerSelection === "Rock" && cumputerSelection === "Rock") {
+    return "Draw"
+  } else if (playerSelection === "Rock" && cumputerSelection === "Paper") {
+    return "you lose ! paper beats rock"
+  } else {
+    return "You win rock beat scissors";
+  }
 }
+const getPlayerChoice = prompt("Rock, Paper, Scissors");
