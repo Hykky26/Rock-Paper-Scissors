72dfbcafeabe13df25c117f6af86c52f4721c2ae
Hykky26 <117548943+Hykky26@users.noreply.github.com>
2022-12-07 00:24:26 +0100

    Feat add the full case for function playRound

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const cumputerChoice = Math.floor(Math.random() * 4);
+const cumputerChoice = Math.floor(Math.random() * 3);
 
-const Rock = "Rock";
-const Paper = "Paper";
-const Scissors = "Scissors";
+const Rock = "ROCK";
+const Paper = "PAPER";
+const Scissors = "SCISOORS";
 
 function getCumputerChoice() {
-  if (cumputerChoice === 3) {
+  if (cumputerChoice === 0) {
     return Rock;
-  } else if (cumputerChoice === 2) {
+  } else if (cumputerChoice === 1) {
     return Paper;
   } else {
     return Scissors;
@@ -15,12 +15,20 @@ function getCumputerChoice() {
 }
 
 function playRound(playerSelection, cumputerSelection) {
-  if (playerSelection === "Rock" && cumputerSelection === "Rock") {
-    return "Draw"
-  } else if (playerSelection === "Rock" && cumputerSelection === "Paper") {
-    return "you lose ! paper beats rock"
-  } else {
+  if (playerSelection === cumputerSelection) {
+    return "draw"
+  } else if (playerSelection === "ROCK" && cumputerSelection === "PAPER") {
+    return "you lose ! paper beats rock";
+  } else if (playerSelection === "ROCK" && cumputerSelection === "SCISOORS") {
     return "You win rock beat scissors";
+  } else if (playerSelection === "PAPER" && cumputerSelection === "ROCK") {
+    return "you win ! paper beats rock";
+  } else if (playerSelection === "PAPER" && cumputerSelection === "SCISOORS") {
+    return "you lose ! scisoors beats paper";
+  } else if (playerSelection === "SCISOORS" && cumputerSelection === "PAPER") {
+    return "you win ! scisoors beat paper";
+  } else if (playerSelection === "SCISOORS" && cumputerSelection === "ROCK") {
+    return "you lose ! rock beat scisoors"
   }
 }
 const getPlayerChoice = prompt("Rock, Paper, Scissors");
