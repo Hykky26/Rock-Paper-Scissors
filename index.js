908d1f0bf5f2260545ddbd703827123a3dbc63e6
Hykky26 <117548943+Hykky26@users.noreply.github.com>
2022-12-14 14:10:37 +0100

    finish the code

--- index.js
+++ index.js
@@ -1,34 +1,75 @@
-const cumputerChoice = Math.floor(Math.random() * 3);
-
-const Rock = "ROCK";
-const Paper = "PAPER";
-const Scissors = "SCISOORS";
+const opt = ["rock", "paper", "scisoors"];
 
 function getCumputerChoice() {
-  if (cumputerChoice === 0) {
-    return Rock;
-  } else if (cumputerChoice === 1) {
-    return Paper;
+  const choice = opt[Math.floor(Math.random() * opt.length)];
+  return choice;
+}
+
+function checkWinner(playerSelection, cumputerSelection) {
+  if (playerSelection === cumputerSelection) {
+    return "tie";
+  } else if (
+    (playerSelection === "rock" && cumputerSelection === "scisoors") ||
+    (playerSelection === "scisoors" && cumputerSelection === "paper") ||
+    (playerSelection === "paper" && cumputerSelection === "rock")
+  ) {
+    return "win";
   } else {
-    return Scissors;
+    return "lose";
   }
 }
 
 function playRound(playerSelection, cumputerSelection) {
-  if (playerSelection === cumputerSelection) {
-    return "draw"
-  } else if (playerSelection === "ROCK" && cumputerSelection === "PAPER") {
-    return "you lose ! paper beats rock";
-  } else if (playerSelection === "ROCK" && cumputerSelection === "SCISOORS") {
-    return "You win rock beat scissors";
-  } else if (playerSelection === "PAPER" && cumputerSelection === "ROCK") {
-    return "you win ! paper beats rock";
-  } else if (playerSelection === "PAPER" && cumputerSelection === "SCISOORS") {
-    return "you lose ! scisoors beats paper";
-  } else if (playerSelection === "SCISOORS" && cumputerSelection === "PAPER") {
-    return "you win ! scisoors beat paper";
-  } else if (playerSelection === "SCISOORS" && cumputerSelection === "ROCK") {
-    return "you lose ! rock beat scisoors"
+  const result = checkWinner(playerSelection, cumputerSelection);
+  if (result === "tie") {
+    return "tie";
+  } else if (result === "win") {
+    return `you win ! ${playerSelection} beats ${cumputerSelection}`;
+  } else {
+    return `you lose ${cumputerSelection} beats ${playerSelection}`;
   }
 }
-const getPlayerChoice = prompt("Rock, Paper, Scissors");
+
+function getPlayerChoice() {
+  let validateInput = false;
+  while (validateInput == false) {
+    const choice = prompt("rock, paper, scisoor");
+    if (choice == null) {
+      continue;
+    }
+    const choiceInLower = choice.toLowerCase();
+    if (opt.includes(choiceInLower)) {
+      validateInput = true;
+      return choiceInLower;
+    }
+  }
+}
+
+function game() {
+  let playerScore = 0;
+  let cumputerScore = 0;
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = getPlayerChoice();
+    const cumputerSelection = getCumputerChoice();
+    console.log(playRound(playerSelection, cumputerSelection));
+
+    console.log("----------------");
+    console.log(cumputerSelection);
+
+    if (checkWinner(playerSelection, cumputerSelection) === "win") {
+      playerScore++;
+    } else if (checkWinner(playerSelection, cumputerSelection) === "lose") {
+      cumputerScore++;
+    }
+  }
+  console.log("Game Over");
+  if (playerScore > cumputerScore) {
+    console.log("Player is the winner");
+  } else if (playerScore < cumputerScore) {
+    console.log("Cumputer is the winner");
+  } else {
+    console.log("Score is a tie");
+  }
+}
+
+game();
